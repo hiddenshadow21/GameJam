61acf6f43fd706d242e3b1ea97795e676fb46432
Bartosz Piekarski <21hiddenshadow@gmail.com>
2021-02-19 01:40:41 +0100

    Dodano wartość zdrowie do gracza

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,16 @@ public class PlayerController : MonoBehaviour
     float speedX = 1;
     [SerializeField]
     float speedY = 1;
+    [SerializeField]
+    float maxHealth = 3;
+
+    public float Health { get { return currentHealth; } }
+    float currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -28,5 +33,11 @@ public class PlayerController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical")* speedY * Time.deltaTime;
         var moveDir = new Vector2(horizontal,vertical);
         transform.Translate(moveDir);
+    }
+
+    public void ChangeHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log($"{currentHealth}/{maxHealth}");
     }
 }
